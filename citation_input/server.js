63ff5d26b018ad47f973ef6cd0a1198f5aa83221
NotLiu <aliu@lediur.com>
2021-06-10 10:20:34 -0500

    fix directory stuff again

--- citation_input/server.js
+++ citation_input/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const MongoClient = require("mongodb").MongoClient;
-const dotenv = require("dotenv").config();
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const app = express();
 
